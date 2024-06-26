21c3c8c3dc1ba7296600c7f0b6fe4633209440ff
Dhyego Tourinho <131512335+DhyegoTourinho@users.noreply.github.com>
2024-06-26 12:08:08 -0300

    Atualizando

--- obj/Debug/net8.0/APIDesafio.AssemblyInfo.cs
+++ obj/Debug/net8.0/APIDesafio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIDesafio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a8594dbc378636f57cbf4359b3169f7f8e653a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5aca092154ed684a5b703c012339da569782f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIDesafio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIDesafio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
